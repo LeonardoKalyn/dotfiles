034c07ab210d2f71e4dc1df122cd4c7580c37656
Leonardo Kalyn <Leonardo.Kalyn@gmail.com>
2020-12-29 19:32:32 -0300

    Update install-apps.sh

--- install-apps.sh
+++ install-apps.sh
@@ -11,15 +11,11 @@ then
 fi
 
 echo "→ Installing applications..."
-# Set up Cask, for Homebrew
-brew tap caskroom/cask
-# Set up Caskroom-versions, which allows apps' alternative versions, like Canary
-brew tap caskroom/versions
 # Install all apps listed in the caskfile
-brew cask install $(cat brew/caskfile|grep -v "#")
+brew install $(cat brew/caskfile|grep -v "#")
 
 echo "→ Installing fonts..."
 # Set up Caskroom-fonts
 brew tap caskroom/fonts
 # Install all fonts listed in the fontfile
-brew cask install $(cat brew/fontfile|grep -v "#")
+brew install $(cat brew/fontfile|grep -v "#")
